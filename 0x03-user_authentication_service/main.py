7034bf910daa88c069dbd222063966ba0b43abe3
Wilfex <84175893+wilfex81@users.noreply.github.com>
2023-02-02 15:39:23 +0300

    files

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -3,10 +3,7 @@
 
 import requests
 
-BASE_URL = 'http://localhost:5000'
-EMAIL = "guillaume@holberton.io"
-PASSWD = "b4l0u"
-NEW_PASSWD = "t4rt1fl3tt3"
+
 
 
 def register_user(email: str, password: str) -> None:
